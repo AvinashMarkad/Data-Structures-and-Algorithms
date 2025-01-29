ecfd09e9eecee2eb7b23628e19647202e6b06c14
AvinashMarkad <markadavi3719@gmail.com>
2025-01-29 19:13:41 +0530

    Array & Splice function

---
+++ DSA Part II/Splice/index.js
@@ -0,0 +1,14 @@
+let array = ["hii", "hello", 123, 1, 2, 3];
+
+// array.splice( 1, 3);
+// console.log(array);
+
+// array.splice(1, 3,"hii");
+// console.log(array);
+
+// array.splice(1, 3, "hii", "hello");
+// console.log(array);
+
+array.splice(-1);
+console.log(array);
+
