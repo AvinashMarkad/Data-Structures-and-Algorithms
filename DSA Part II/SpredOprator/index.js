317f6b648b6312f71d94b83268589e073c5be3cf
AvinashMarkad <markadavi3719@gmail.com>
2025-01-31 22:27:15 +0530

    SpredOprator

---
+++ DSA Part II/SpredOprator/index.js
@@ -0,0 +1,8 @@
+let marval_heros = ["iron man", "thor", "hulk"]
+let dc_heros = ["flash", "superman"]
+
+// console.log(marval_heros)
+// console.log(dc_heros)
+
+let newArray = [...marval_heros, ...dc_heros]
+console.log(newArray)
